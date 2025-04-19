bf5faf533e60c259e025cee12a2f39211cac0bab
mark-pranav <pranavbobade48@gmail.com>
2025-04-19 13:11:06 +0530

    res1

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-
-
+import os
 from dotenv import load_dotenv
-load_dotenv()
-
-
 from app.main import app
 
+load_dotenv()
 
-
-# Run using: uvicorn run:app --reload
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))  # Render sets PORT env variable
+    uvicorn.run("run:app", host="0.0.0.0", port=port, reload=True)
