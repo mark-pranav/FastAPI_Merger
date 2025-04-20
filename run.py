e213ccf53a04776784968501dc6736167a260556
mark-pranav <pranavbobade48@gmail.com>
2025-04-20 21:04:16 +0530

    port update

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-import os
-import uvicorn
+
 from app.main import app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Use Render's PORT or fallback to 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
